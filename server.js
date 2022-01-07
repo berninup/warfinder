0584af7648bb6384506eb54f43536043fb284380
chris-basham <chris.basham@live.com>
2022-01-07 11:23:06 -0600

    index page routed and linked

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
-const methodOverride = require('method-override')
+require('dotenv').config();
+const methodOverride = require('method-override');
+const indexRouter = require('./controllers');
+const morgan = require('morgan')
 
 
-app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:false}));
 app.use(methodOverride('_method'));
 app.use(express.static('public'))
 
-
+app.use(indexRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log('here'))
